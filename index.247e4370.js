604a38e0d6f23b07ec2fa3edee4e2a278c6c8238
goit-latam <126070957+goit-latam@users.noreply.github.com>
2023-06-06 18:27:55 +0000

    Deploying to gh-pages from @ goit-latam/modulo-8-clase-2@bbf2ccee0477bfdd779ef9cb7d8787300d91dae6 🚀

---
+++ index.247e4370.js
@@ -0,0 +1,2 @@
+localStorage.setItem("user",JSON.stringify({userName:"Andrii",userAge:40,children:["Matviy","Marko"],pass:"qwerty"})),localStorage.getItem("user"),console.log(JSON.parse(localStorage.getItem("user"))),console.log(window);
+//# sourceMappingURL=index.247e4370.js.map
---
+++ index.ef0da4b1.js
@@ -0,0 +1,2 @@
+localStorage.setItem("user",JSON.stringify({userName:"Andrii",userAge:40,children:["Matviy","Marko"],pass:"qwerty"})),localStorage.getItem("user"),console.log(JSON.parse(localStorage.getItem("user"))),console.log(window);
+//# sourceMappingURL=index.ef0da4b1.js.map

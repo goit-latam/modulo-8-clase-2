13f55f890621f52376c1c6c0ff403bc1c3c58620
goit-latam <126070957+goit-latam@users.noreply.github.com>
2023-06-06 20:26:27 +0200

    done

---
+++ src/js/math.js
@@ -0,0 +1,26 @@
+'use strict';
+
+export const add = (a, b) => {
+  return a + b;
+};
+
+export const sub = (a, b) => {
+  return a - b;
+};
+
+export const division = (a, b) => {
+  return a / b;
+};
+
+export const multi = (a, b) => {
+  return a * b;
+};
+
+const data = 10;
+
+export default multi;
+
+// export default  math = {
+//   add,
+//   sub,
+// };
